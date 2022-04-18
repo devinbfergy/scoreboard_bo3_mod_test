672c0578408234930bb7f4167ebab320c407ac65
devintenfold <devin@tenfoldsecurity.com>
2022-04-17 20:42:51 -0500

    adding inmportant sttuffa

---
+++ scripts/mp/scoreboard_print/luatest.lua
@@ -0,0 +1,10 @@
+require("ui.util.T7Overcharged")
+
+InitializeT7Overcharged({
+	modname = "scoreboard_test",
+	filespath = [[.\mods\mp_scoreboard\]],
+	workshopid = "2795402786",
+	discordAppId = nil
+})
+
+T7Overcharged
